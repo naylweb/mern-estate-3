6894f415a279b0bddc4ae03993541e73310cffdf
nyel <naylwebthe@gmail.com>
2025-03-19 23:50:10 +0300

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Signin } from "./pages/Signin";
+import { SignUp } from "./pages/SignUp";
+import { About } from "./pages/About";
+import { Profile } from "./pages/Profile";
 
 export default function App() {
-  return <div className="text-red-600">App</div>;
+  return (
+    <Routes>
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Routes>
+  );
 }
